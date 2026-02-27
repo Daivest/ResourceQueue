753f68c0980f561fbb059b7cd7217a0d989c38df
Andrew Lazarev <andrej.lazarev@gmail.com>
2026-02-27 17:38:48 +0500

    Initial Commit

---
+++ ResourceQueueApp/ResourceQueueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResourceQueueAppApp.swift
+//  ResourceQueueApp
+//
+//  Created by Andrew on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ResourceQueueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResourceQueueAppTests/ResourceQueueAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResourceQueueAppTests.swift
+//  ResourceQueueAppTests
+//
+//  Created by Andrew on 27.02.2026.
+//
+
+import Testing
+@testable import ResourceQueueApp
+
+struct ResourceQueueAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
